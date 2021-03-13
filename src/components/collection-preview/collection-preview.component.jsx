29adda9d999872be377d3e12a45a94ed2cb7c91b
kris-gergov <krisgergov@gmail.com>
2021-03-13 13:58:50 +0000

    Updated shop data, add cart utills to group items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,12 +6,12 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
 	<div className="collection-preview">
-		<h1 classNam="title">{title.toUpperCase()}</h1>
+		<h1 className="title">{title.toUpperCase()}</h1>
 		<div className="preview">
 			{items
 				.filter((item, index) => index < 4)
 				.map(item => (
-					<CollectionItem key={item.id} name={item.name} imageUrl={item.imageUrl} price={item.price} />
+					<CollectionItem key={item.id} item={item} />
 				))}
 		</div>
 	</div>
